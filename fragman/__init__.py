737c4b1e831d6209aaac1998df512e6f695da7d1
Matt Chisholm <matt@theory.org>
2012-05-10 22:28:54 +0200

    basic run command framework and configuration directory finder

--- fragman/__init__.py
+++ fragman/__init__.py
@@ -1,1 +1,18 @@
+import os
 __version__ = (0,0,1)
+
+configuration_name = 'fragments'
+
+class FragmanException(Exception): pass
+class ConfigurationNotFound(FragmanException): pass
+
+
+def find_configuration(current=None):
+    path = current or os.getcwd()
+    while True:
+        configuration_path = os.path.join(path, configuration_name)
+        if os.path.exists(current) and os.path.exists(configuration_path):
+            return configuration_path
+        path, oldpath = os.path.split(path)[0], path
+        if oldpath == path:
+            raise ConfigurationNotFound(current)
--- fragman/__main__.py
+++ fragman/__main__.py
@@ -1,4 +1,17 @@
-import sys, os
+import sys, os, pdb
 
-from fragman import __version__
-print "%s version %s.%s.%s" % ((__package__,) + __version__)
+from fragman import __version__, find_configuration, ConfigurationNotFound
+
+
+def help(*a):
+    print "help!"
+
+
+def init(*a):
+    find_configuration()
+
+
+if __name__ == '__main__':
+    print "%s version %s.%s.%s" % ((__package__,) + __version__)
+    locals().get(sys.argv[1], help)(sys.argv[2:])
+
