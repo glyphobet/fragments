f2801444b2735e8b0f3a0d5050d19d3bbb979559
matt chisholm <matt@theory.org>
2012-05-24 21:07:42 +0200

    setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import fragments
+
+setup(
+    name='fragments',
+    version='.'.join(map(str, fragments.__version__)),
+    description='Differencing Fragmentation Manager',
+    long_description=open('README.md').read(),
+    author='Matt Chisholm',
+    author_email='matt-fragments@theory.org',
+    # url='https://github.com/glyphobet/fragments',
+    packages= ['fragments',],
+    entry_points = {
+        'console_scripts': [
+            'fragments = fragments.commands:main',
+        ],
+    },
+    license='BSD',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
