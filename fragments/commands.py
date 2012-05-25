6f3340c7e7e6dd571d9b6dfb0b21a92f18217553
Matt Chisholm <matt@theory.org>
2012-05-26 00:27:19 +0200

    splitting up some logic

--- fragments/commands.py
+++ fragments/commands.py
@@ -121,6 +121,7 @@ def _iterate_over_files(args, config):
 
 
 def _visible_in_diff(merge_result, context_lines=3):
+    """Collects the set of lines that should be visible in a diff with a certain number of context lines"""
     i = old_line = new_line = 0
     while i < len(merge_result):
         line_or_tuple = merge_result[i]
@@ -148,7 +149,8 @@ def _visible_in_diff(merge_result, context_lines=3):
     yield None
 
 
-def _diff_groups(merge_result, context_lines=3):
+def _split_diff(merge_result, context_lines=3):
+    """Split diffs and context lines into groups based on None sentinel"""
     collect = []
     for item in _visible_in_diff(merge_result, context_lines=context_lines):
         if item is None:
@@ -159,41 +161,53 @@ def _diff_groups(merge_result, context_lines=3):
             collect.append(item)
 
 
+def _diff_group_position(group):
+    """Generate a unified diff position line for a diff group"""
+    old_start = group[0][0]
+    new_start = group[0][1]
+    old_length = new_length = 0
+    for old_line, new_line, line_or_tuple in group:
+        if isinstance(line_or_tuple, tuple):
+            old, new = line_or_tuple
+            old_length += len(old)
+            new_length += len(new)
+        else:
+            old_length += 1
+            new_length += 1
+    if old_length:
+        old_start += 1
+    if new_length:
+        new_start += 1
+
+    return '@@ -%s,%s +%s,%s @@\n' % (old_start, old_length, new_start, new_length)
+
+
+def _diff_group(group):
+    """Generate a diff section for diff group"""
+    yield _diff_group_position(group)
+
+    for old_line, new_line, line_or_tuple in group:
+        if isinstance(line_or_tuple, tuple):
+            old, new = line_or_tuple
+            for o in old:
+                yield '-' + o
+            for n in new:
+                yield '+' + n
+        else:
+            yield ' ' + line_or_tuple
+
+
 def _full_diff(merge_result, key, context_lines=3):
+    """Generate a full diff based on a Weave merge result"""
     header_printed = False
-    for group in _diff_groups(merge_result, context_lines=context_lines):
+    for group in _split_diff(merge_result, context_lines=context_lines):
         if not header_printed:
             header_printed = True
             yield '--- %s\n' % key
             yield '+++ %s\n' % key
 
-        old_start = group[0][0]
-        new_start = group[0][1]
-        old_length = new_length = 0
-        for old_line, new_line, line_or_tuple in group:
-            if isinstance(line_or_tuple, tuple):
-                old, new = line_or_tuple
-                old_length += len(old)
-                new_length += len(new)
-            else:
-                old_length += 1
-                new_length += 1
-        if old_length:
-            old_start += 1
-        if new_length:
-            new_start += 1
-
-        yield '@@ -%s,%s +%s,%s @@\n' % (old_start, old_length, new_start, new_length)
-
-        for old_line, new_line, line_or_tuple in group:
-            if isinstance(line_or_tuple, tuple):
-                old, new = line_or_tuple
-                for o in old:
-                    yield '-' + o
-                for n in new:
-                    yield '+' + n
-            else:
-                yield ' ' + line_or_tuple
+        for l in _diff_group(group):
+            yield l
 
 
 def diff(*args):
