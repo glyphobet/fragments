e020f1da5654a124446bee88b0f7c12d746b23bc
matt chisholm <matt@theory.org>
2012-05-31 02:42:05 +0200

    colorize diff, +++, and --- lines with bold

--- fragments/color.py
+++ fragments/color.py
@@ -1,19 +1,21 @@
 import sys
 
-GREY    = 90
-RED     = 91
-GREEN   = 92
-YELLOW  = 93
-BLUE    = 94
-MAGENTA = 95
-CYAN    = 96
-WHITE   = 97
-
+GREY    = 30
+RED     = 31
+GREEN   = 32
+YELLOW  = 33
+BLUE    = 34
+MAGENTA = 35
+CYAN    = 36
+WHITE   = 37
+BRIGHT_WHITE = 57
+NORMAL = 0
+BOLD = 1
 
 def colorize(line, color=None, colorblind=False):
     color_by_prefix = {
-        '+' : BLUE if colorblind else GREEN,
-        '-' : RED    ,
+        '+' : (BLUE if colorblind else GREEN),
+        '-' : RED,
         '@' : MAGENTA,
         '?' : MAGENTA,
         'M' : YELLOW ,
@@ -21,9 +23,15 @@ def colorize(line, color=None, colorblind=False):
         'A' : BLUE if colorblind else GREEN,
         'E' : GREY   ,
     }
+    weight = NORMAL
     if color is None:
-        color = color_by_prefix.get(line[0:1])
-    if sys.stdout.isatty() and color:
-        return '\033[%sm%s\033[0m' % (color, line)
+        color = color_by_prefix.get(line[0:1], WHITE)
+    if line.startswith('diff '):
+        color = BRIGHT_WHITE
+        weight = BOLD
+    if line.startswith('+++') or line.startswith('---'):
+        weight = BOLD
+    if color and sys.stdout.isatty():
+        return '\033[%sm\033[%sm%s\033[0m' % (weight, color, line)
     else:
         return line
