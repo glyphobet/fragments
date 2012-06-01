f434896439daaeb7b2dd35782aedcdba1cf762f1
matt chisholm <matt@theory.org>
2012-06-01 09:11:46 +0200

    slightly better completion

--- fragments-completion.bash
+++ fragments-completion.bash
@@ -1,27 +1,27 @@
-# file: UseGetOpt-2
-# UseGetOpt-2.sh parameter-completion
-
 _UseFragments ()   # By convention, the function name
 {                  # starts with an underscore.
-  local cur prev
+  local curr prev cmd
   # Pointer to current completion word.
   # By convention, it's named "cur" but this isn't strictly necessary.
 
   COMPREPLY=()   # Array variable storing the possible completions.
   curr=${COMP_WORDS[COMP_CWORD]}
   prev=${COMP_WORDS[COMP_CWORD-1]}
+  if [ $COMP_CWORD > 2 ] ; then
+    cmd=${COMP_WORDS[1]}
+  fi
 
-  if [ "$prev" == "apply" ] ; then
+  if [ "$cmd" == "apply" ] ; then
       case "$curr" in
         -*)
             COMPREPLY=( $( compgen -W '-i -a -U --unified' -- $curr ) );;
       esac;
-  elif [ "$prev" == "diff" -o "$prev" == "fork" ] ; then
+  elif [ "$cmd" == "diff" -o "$prev" == "fork" ] ; then
       case "$curr" in
         -*)
             COMPREPLY=( $( compgen -W '-U --unified' -- $curr ) );;
       esac;
-  elif [ "$prev" == "fragments" -o "$prev" == "help" ] ; then
+  elif [ "$prev" == "fragments" -o "$cmd" == "help" ] ; then
       case "$curr" in
         a*)
             COMPREPLY=( $( compgen -W 'apply' -- $curr ) );;
