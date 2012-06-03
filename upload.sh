28a9abafbf749dcaf597b6113a69de03e0f5a9bd
Matt Chisholm <matt@theory.org>
2012-06-03 15:48:32 +0200

    add PyPI documentation building to upload script

--- upload.sh
+++ upload.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 python2.7 setup.py bdist_egg register upload
 python2.7 setup.py sdist     register upload
+
+markdown_py README.md > index.html
+rm pypi-docs.zip
+zip pypi-docs.zip index.html
+rm index.html
+echo "Now upload pypi-docs.zip to PyPi here: http://pypi.python.org/pypi?%3Aaction=pkg_edit&name=fragments"
+echo "Remember to remove pypi-docs.zip when you're done."
